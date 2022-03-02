55d53917cea5609b897ecfa6db8b20689830272e
wahyu_illahi07 <64129404+Wahyuillahi123@users.noreply.github.com>
2022-03-02 09:08:38 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// # Support
+// Follow my instagram : https://www.instagram.com/wahyu_illahi07/ 
+// Youtube Channel : https://www.youtube.com/playlist?list=PL2DC6n3PrEKVXN9nvzONe9idXa9BgCusj
+
+var action='';
+
+if(action=='like'){
+    alert('Thank You :)');
+}
